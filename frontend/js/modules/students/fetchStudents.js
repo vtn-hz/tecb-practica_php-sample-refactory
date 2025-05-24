9200a35a0853ab023d166321001cbc98ed2b7da9
vtn-hz <juaniyvalen1234@gmail.com>
2025-05-24 11:31:01 -0300

    fea: widget tbody sample

--- frontend/js/modules/students/fetchStudents.js
+++ frontend/js/modules/students/fetchStudents.js
@@ -1,59 +1,30 @@
-import { create }
+import { createTbody } from "../../widgets/tbody.js";
 
 async function fetchStudents() {
     const API_URL = getEndpoint();
     const elements = await getElements();
 
-    try {
-        const response = await get(API_URL);
-        const students = response.data;
-        //Limpiar tabla de forma segura.
-        elements.studentTableBody.replaceChildren();
-        //acá innerHTML es seguro a XSS porque no hay entrada de usuario
-        //igual no lo uso.
-        //studentTableBody.innerHTML = "";
+    //try {
+        const tbody = createTbody(elements.studentTableBody, {
+            fields: ['fullname', 'email', 'age'],
+            sourceUrl: API_URL,
+            actionHandlers: {
+                edit: event => {
+                    console.log(event.target);
+                    // change... i cant send id neither dataform
+                },
 
-        students.forEach(student => {
-            const tr = document.createElement('tr');
-
-            const tdName = document.createElement('td');
-            tdName.textContent = student.fullname;
-
-            const tdEmail = document.createElement('td');
-            tdEmail.textContent = student.email;
-
-            const tdAge = document.createElement('td');
-            tdAge.textContent = student.age;
-
-            const tdActions = document.createElement('td');
-            const editBtn = document.createElement('button');
-            editBtn.textContent = 'Editar';
-            editBtn.classList.add('w3-button', 'w3-blue', 'w3-small', 'w3-margin-right');
-            editBtn.onclick = () => {
-                elements.fullnameInput.value = student.fullname;
-                elements.emailInput.value = student.email;
-                elements.ageInput.value = student.age;
-                elements.studentIdInput.value = student.id;
-            };
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.textContent = 'Borrar';
-            deleteBtn.classList.add('w3-button', 'w3-red', 'w3-small');
-            deleteBtn.onclick = () => deleteStudent(student.id);
-
-            tdActions.appendChild(editBtn);
-            tdActions.appendChild(deleteBtn);
-
-            tr.appendChild(tdName);
-            tr.appendChild(tdEmail);
-            tr.appendChild(tdAge);
-            tr.appendChild(tdActions);
-
-            elements.studentTableBody.appendChild(tr);
+                remove: event => {
+                    console.log(event.target); 
+                    // change... i cant send id neither dataform
+                }
+            }
         });
-    } catch (err) {
-        console.error("Error al obtener estudiantes:", err);
-    }
+
+        tbody.render();
+    //} catch (err) {
+        //console.error("Error al obtener estudiantes:", err);
+    //}
 }
 
 fetchStudents();
